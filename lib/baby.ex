151f2f3dee7c61079ae36aa7887d2c5778835942
Matthew Miller <mwm@fastmail.com>
2022-08-08 09:06:18 +0200

    Allow connecting out on a different identity
    We might want to accept connections on one identity and connect to
    others on another.  For some reason.

--- lib/baby.ex
+++ lib/baby.ex
@@ -1,22 +1,29 @@
 defmodule Baby do
   @moduledoc """
-  Documentation for `Baby`.
+  Bushbaby Automated Bamboo Yields
   """
 
-  def connect(host, port) when is_binary(port), do: connect(host, String.to_integer(port))
+  @doc """
+  Connect to a remote hort and port with a given identity
+  The confiured identity is the default
+  """
+  def connect(host, port), do: connect(host, port, Application.get_env(:baby, :identity))
 
-  def connect(host, port) when is_binary(host) do
+  def connect(host, port, identity) when is_binary(port),
+    do: connect(host, String.to_integer(port), identity)
+
+  def connect(host, port, identity) when is_binary(host) do
     case :inet.gethostbyname(to_charlist(host)) do
-      {:ok, {:hostent, _, _, _, _, [addr | _more]}} -> connect(addr, port)
+      {:ok, {:hostent, _, _, _, _, [addr | _more]}} -> connect(addr, port, identity)
       _ -> :error
     end
   end
 
-  def connect(host, port) do
+  def connect(host, port, identity) do
     Baby.Connection.start_link(
       host: host,
       port: port,
-      identity: Application.get_env(:baby, :identity)
+      identity: identity
     )
   end
 end
