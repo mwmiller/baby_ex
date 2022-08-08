cf04a8281eb1b8b6b927c1cc975460654576cee4
Matthew Miller <mwm@fastmail.com>
2022-08-08 08:49:08 +0200

    Ensure minimal level of environment sanity
    This is not kind of gaurantee, but it at least tries to fix things where
    the solutions are obvious.
    
    In theory, another application has been using the Baobab store.

--- lib/baby/application.ex
+++ lib/baby/application.ex
@@ -7,12 +7,30 @@ defmodule Baby.Application do
 
   @impl true
   def start(_type, _args) do
+    # Ensure the startup environment makes some sense
+    # The configured spool dir must exist we make no assumptions
+    # about its suitability
+    proper_dir = Application.get_env(:baobab, :spool_dir) |> Path.expand()
+
+    case File.exists?(proper_dir) do
+      true -> :ok
+      false -> File.mkdir_p(proper_dir)
+    end
+
+    # The configured identity must exist
+    whoami = Application.get_env(:baby, :identity)
+
+    case Baobab.identity_key(whoami, :public) do
+      :error -> Baobab.create_identity(whoami)
+      _ -> :ok
+    end
+
     :ranch.start_listener(
       :baby,
       :ranch_tcp,
       [port: Application.get_env(:baby, :port)],
       Baby.Connection,
-      Application.get_env(:baby, :identity)
+      whoami
     )
 
     children = [
