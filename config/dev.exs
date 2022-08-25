34331320b8229aa40ac9712976db91710bdb42d4
Matthew Miller <mwm@fastmail.com>
2022-08-25 18:26:18 +0200

    Cryout to zebra
    I do not intend to keep zebra moving as quickly as quagga.  So it will
    be a good regression test.  Although keeping these configs in the git
    repo is a bit suboptimal.  Good enough for now.
    
    Also includes an update to improve ~shortname matching from `Baobab`
    
    I also wanted to see how difficult it was to set up a new instance.
    Not bad.

--- config/dev.exs
+++ config/dev.exs
@@ -7,4 +7,7 @@ config :baby,
   identity: "mwm",
   clump_id: "Quagga",
   port: 8483,
-  cryouts: [[host: "quagga.nftease.online", port: 8483, period: {7, :minute}]]
+  cryouts: [
+    [host: "quagga.nftease.online", port: 8483, period: {7, :minute}],
+    [host: "zebra.nftease.online", port: 8483, period: {17, :minute}]
+  ]
