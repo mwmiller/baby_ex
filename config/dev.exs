733de05d833da0edb43acc2e8efb2f1570345354
Matthew Miller <mwm@fastmail.com>
2022-10-17 12:15:40 +0200

    Include extra clumps for testing

--- config/dev.exs
+++ config/dev.exs
@@ -11,5 +11,13 @@ config :baby,
         [host: "quagga.nftease.online", port: 8483, period: {7, :minute}],
         [host: "zebra.nftease.online", port: 8483, period: {17, :minute}]
       ]
+    ],
+    [
+      id: "Pitcairn",
+      controlling_identity: "mwm",
+      port: 8483,
+      cryouts: [
+        [host: "sally.nftease.online", port: 8485, period: {7, :minute}]
+      ]
     ]
   ]
