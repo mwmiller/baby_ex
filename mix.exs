f2fe422932c761534f3dfbf50f4fe27d05e4b79e
Matthew Miller <mwm@fastmail.com>
2023-08-03 09:52:50 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Baby.MixProject do
   def project do
     [
       app: :baby,
-      version: "0.9.6",
+      version: "0.10.0",
       elixir: "~> 1.13",
       name: "Baby",
       source_url: "https://github.com/mwmiller/baby_ex",
