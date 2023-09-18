8265e5a1c9f817f994a6bdb26b28448bae128ff3
Matthew Miller <mwm@fastmail.com>
2023-09-18 10:23:26 +0200

    Update deps
    We always should have had blake2 in our own deps, but now it is missing
    from the other deps
    
    My elixir package is actually better in that it allows for the short
    hashing. Which is decidely not the same as a truncated long hash.
    
    And so it stays for our purposes.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Baby.MixProject do
   def project do
     [
       app: :baby,
-      version: "0.22.0",
+      version: "0.23.0",
       elixir: "~> 1.13",
       name: "Baby",
       source_url: "https://github.com/mwmiller/baby_ex",
@@ -24,7 +24,8 @@ defmodule Baby.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:baobab, "~> 0.19.0"},
+      {:baobab, "~> 0.20.0"},
+      {:blake2, "~> 1.0"},
       {:primacy,">= 0.0.0"},
       {:stlv, "~> 1.0"},
       {:varu64, "~> 1.0"},
