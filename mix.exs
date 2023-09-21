297b69545b3ca258adc81f3521844fea8b99ac83
Matthew Miller <mwm@fastmail.com>
2023-09-21 14:49:19 +0200

    Inc baobab

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Baby.MixProject do
   def project do
     [
       app: :baby,
-      version: "0.30.0",
+      version: "0.30.1",
       elixir: "~> 1.15",
       name: "Baby",
       source_url: "https://github.com/mwmiller/baby_ex",
